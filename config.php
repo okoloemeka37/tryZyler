fb87236c8f2a4990ac8124fb7eb78adb4de10f4f
okoloemeka37 <97386408+okoloemeka37@users.noreply.github.com>
2024-08-29 22:03:45 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$connect=mysqli_connect('localhost','root','','try');
+$connect=mysqli_connect('bb0sms6y9q7sw7p4i6hh-mysql.services.clever-cloud.com','ummfm1sojzalqjqi','R8vSerRCPMlLjwTp1VIw','bb0sms6y9q7sw7p4i6hh');
 
 
 session_start();
