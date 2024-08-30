18f9da127b681e1a30e1a4700d5c570a519b3e8c
okoloemeka37 <97386408+okoloemeka37@users.noreply.github.com>
2024-08-30 13:35:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,30 @@ if(isset($_SESSION['user'])){
 
 require_once('auth.php');
 
+// set this to the twitter handle you want to get follower count for
+	$twitterHandle = 'zylercodes';
+	
+	// initialize cURL session
+	$curl_handle=curl_init();
+	
+	// set the URL of the page to download
+	curl_setopt($curl_handle,CURLOPT_URL,'https://api.twitter.com/1/users/show.json?screen_name=' . $twitterHandle . '&include_entities=true');
+	
+	// set the timeout to 2 seconds
+	curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2);
+	
+	// Ask cURL to return the contents in a variable instead of simply echoing them to  the browser
+	curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+	
+	// execute the cURL session and set result as a string
+	$result = curl_exec($curl_handle);
+	
+	// close the cURL session
+	curl_close($curl_handle);
 
-$url = 'http://twitter.com/users/show/zylercodes';
-$response = file_get_contents ( $url );
-var_dump($response);
-
-//$t_profile = new SimpleXMLElement ( $response );
-//$count = $t_profile->followers_count;
-
-//echo $count;
-
+	// use php to break up json string into array
+	$array = json_decode($result, true);
+var_dump($array);
 ?>
 
 <!DOCTYPE html>
