f908487221cf5386e09c40443972ddee7e17f7b6
okoloemeka37 <97386408+okoloemeka37@users.noreply.github.com>
2024-08-31 20:14:56 +0100

    Update task.php

--- task.php
+++ task.php
@@ -1,3 +1,7 @@
+
+<?php require_once("config.php")?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+    <?php require_once("navbar.php")?>
     <div class="container mt-4">
         <h1 class="mb-4">Task Manager</h1>
 
